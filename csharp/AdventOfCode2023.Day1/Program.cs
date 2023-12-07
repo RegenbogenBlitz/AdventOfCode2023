30e454ed13adefc885f88bedaa73d18977bf9dc1
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2023-12-07 22:58:16 +0000

    solve day 1 part 2 csharp

--- csharp/AdventOfCode2023.Day1/Program.cs
+++ csharp/AdventOfCode2023.Day1/Program.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode2023.Day1;
 
 public static class Program
 {
-    public static int SumFileNumbers(string filename)
+    public static int Part1(string filename)
     {
         var total = 0;
         var lines = File.ReadAllLines(filename);
@@ -18,6 +18,67 @@ public static class Program
             var lastNumber = numbers.LastOrDefault();
             var sum = firstNumber * 10 + lastNumber;
             total += sum;
+        }
+
+        return total;
+    }
+    
+    public static int Part2(string filename)
+    {
+        var numberWords = new[]
+        {
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9)
+        };
+        
+        var total = 0;
+        var lines = File.ReadAllLines(filename);
+        foreach (var line in lines)
+        {
+            var chars = line.ToCharArray();
+
+            var firstNumber = (int?)null;
+            var lastNumber = (int?)null;
+            for (var i = 0; i < chars.Length; i++)
+            {
+                var c = chars[i];
+                if (char.IsDigit(c))
+                {
+                    firstNumber ??= int.Parse(c.ToString());
+                    lastNumber = int.Parse(c.ToString());
+                    continue;
+                }
+
+                foreach (var numberWord in numberWords)
+                {
+                    var word = numberWord.Item1;
+                    var number = numberWord.Item2;
+                    if (i + word.Length > chars.Length)
+                    {
+                        continue;
+                    }
+                    
+                    var wordChars = new string(chars.Skip(i).Take(word.Length).ToArray());
+                    if (word == wordChars)
+                    {
+                        firstNumber ??= number;
+                        lastNumber = number;
+                    }
+                }
+            }
+            
+            if(firstNumber is not null && lastNumber is not null)
+            {
+                var sum = firstNumber.Value * 10 + lastNumber.Value;
+                total += sum;
+            }
         }
 
         return total;
--- csharp/AdventOfCode2023.Day1/UnitTest1.cs
+++ csharp/AdventOfCode2023.Day1/UnitTest1.cs
@@ -8,10 +8,34 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void Part1Example()
     {
-        var total = Program.SumFileNumbers("example1.txt");
+        var total = Program.Part1("example1.txt");
         
         Assert.That(total, Is.EqualTo(142));
+    }
+    
+    [Test]
+    public void Part1Solution()
+    {
+        var total = Program.Part1("input.txt");
+        
+        Assert.That(total, Is.EqualTo(54081));
+    }
+    
+    [Test]
+    public void Part2Example()
+    {
+        var total = Program.Part2("example2.txt");
+        
+        Assert.That(total, Is.EqualTo(281));
+    }
+    
+    [Test]
+    public void Part2Solution()
+    {
+        var total = Program.Part2("input.txt");
+        
+        Assert.That(total, Is.EqualTo(54649));
     }
 }
