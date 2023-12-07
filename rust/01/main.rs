7e9861092df7d5400b827124479ddde651ead6e6
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2023-12-07 21:57:04 +0000

    solve part 1 day 1

--- rust/01/main.rs
+++ rust/01/main.rs
@@ -1,11 +1,12 @@
 use std::fs;
 
 fn main(){
-    let filename = "example.txt";
+    let filename = "puzzle1input.txt";
     match fs::read_to_string(filename)
     {
         Ok(contents)=>{
             let lines = contents.lines();
+            let mut total = 0;
             for (_, line) in lines.enumerate() {
                 let mut first_digit_char : Option<char> = None;
                 let mut last_digit_char: Option<char> = None;
@@ -19,25 +20,21 @@ fn main(){
                 match first_digit_char
                 {
                     Some(digit_char)=>{
-                        print!("First {}", digit_char);
+                        total += 10 * digit_char.to_digit(10).unwrap();
                     },
-                    None =>{
-
-                        print!("None");
-                    }
+                    None =>{}
                 }
                 
                 match last_digit_char
                 {
                     Some(digit_char)=>{
-                        println!(" Last {}", digit_char);
+                        total += digit_char.to_digit(10).unwrap();
                     },
-                    None =>{
-
-                        println!(" None");
-                    }
+                    None =>{}
                 }
             }
+            
+            println!("Total: {}", total);
         },
         Err(error)=>{
             println!("Failed to read file: {}", error);
