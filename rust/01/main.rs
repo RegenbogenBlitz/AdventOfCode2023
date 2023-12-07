05732a7f63cd3e81cbb8ca417185f9488479de51
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2023-12-07 21:57:18 +0000

    wip part 2 day 1

--- rust/01/main.rs
+++ rust/01/main.rs
@@ -1,36 +1,168 @@
 use std::fs;
 
 fn main(){
-    let filename = "puzzle1input.txt";
+    let filename = "example2.txt";
     match fs::read_to_string(filename)
     {
         Ok(contents)=>{
             let lines = contents.lines();
             let mut total = 0;
+            
+
             for (_, line) in lines.enumerate() {
-                let mut first_digit_char : Option<char> = None;
-                let mut last_digit_char: Option<char> = None;
-                for digit in line.chars().filter(|c| c.is_digit(10)){
-                    if first_digit_char == None {
-                        first_digit_char = Some(digit);
-                    }
-                    last_digit_char = Some(digit);
+                let mut first_digit : Option<u32> = None;
+                let mut last_digit: Option<u32> = None;
+
+                let mut line_chars_enum = line.chars();
+                let mut character = line_chars_enum.next();
+                if character == None {
+                    break;
                 }
 
-                match first_digit_char
+                loop
                 {
-                    Some(digit_char)=>{
-                        total += 10 * digit_char.to_digit(10).unwrap();
-                    },
-                    None =>{}
+                   let mut digit: Option<u32> = None;
+
+                    loop{
+                        if character.unwrap().is_digit(10) {
+                            let character = character.unwrap();
+                            print!("{} ",character);
+                            digit = character.to_digit(10);
+                        }
+
+                        match character {
+                            Some('o')=>{
+                                character = line_chars_enum.next();
+                                if character == Some('n') {
+                                    character = line_chars_enum.next();
+                                    if character == Some('e') {
+                                        digit = Some(1);
+                                        print!("one ");
+                                    }
+                                }
+                            },
+                            Some('t') =>{
+                                character = line_chars_enum.next();
+                                if character == Some('w') {
+                                    character = line_chars_enum.next();
+                                    if character == Some('o') {
+                                        digit = Some(2);
+                                        print!("two ");
+                                    }
+                                }
+                                else if character == Some('h') {
+                                    character = line_chars_enum.next();
+                                    if character == Some('r') {
+                                        character = line_chars_enum.next();
+                                        if character == Some('e') {
+                                            character = line_chars_enum.next();
+                                            if character == Some('e') {
+                                                digit = Some(3);
+                                                print!("three ");
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            //Some('f') =>{
+                            //    character = line_chars_enum.next();
+                            //    if character == Some('o') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('u') {
+                            //            character = line_chars_enum.next();
+                            //            if character == Some('r') {
+                            //                digit = Some(4);
+                            //                print!("four ");
+                            //            }
+                            //        }
+                            //    }
+                            //    else if character == Some('i') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('v') {
+                            //            character = line_chars_enum.next();
+                            //            if character == Some('e') {
+                            //                digit = Some(5);
+                            //                print!("five ");
+                            //            }
+                            //        }
+                            //    }
+                            //},
+                            //Some('s') =>{
+                            //    character = line_chars_enum.next();
+                            //    if character == Some('i') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('x') {
+                            //            digit = Some(6);
+                            //            print!("six ");
+                            //        }
+                            //    }
+                            //    else if character == Some('e') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('v') {
+                            //            character = line_chars_enum.next();
+                            //            if character == Some('e') {
+                            //                character = line_chars_enum.next();
+                            //                if character == Some('n') {
+                            //                    digit = Some(7);
+                            //                    print!("seven ");
+                            //                }
+                            //            }
+                            //        }
+                            //    }
+                            //},
+                            //Some('e')=>{
+                            //    character = line_chars_enum.next();
+                            //    if character == Some('i') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('g') {
+                            //            character = line_chars_enum.next();
+                            //            if character == Some('h') {
+                            //                character = line_chars_enum.next();
+                            //                if character == Some('t') {
+                            //                    digit = Some(8);
+                            //                    print!("eight ");
+                            //                }
+                            //            }
+                            //        }
+                            //    }
+                            //},
+                            //Some('n') =>{
+                            //    if character == Some('i') {
+                            //        character = line_chars_enum.next();
+                            //        if character == Some('n') {
+                            //            character = line_chars_enum.next();
+                            //            if character == Some('e') {
+                            //                digit = Some(9);
+                            //                print!("nine ");
+                            //            }
+                            //        }
+                            //    }
+                            //},
+                            _ => { break; }
+                        }
+
+                        if let Some(_) = digit {
+                            if first_digit == None {
+                                first_digit = digit;
+                            }
+                            last_digit = digit;
+                        }
+                    }
+
+                    character = line_chars_enum.next();
+                    if character == None {
+                        break;
+                    }
+
                 }
                 
-                match last_digit_char
-                {
-                    Some(digit_char)=>{
-                        total += digit_char.to_digit(10).unwrap();
-                    },
-                    None =>{}
+                println!(" : {}", line);
+
+                if let Some(digit) = first_digit {
+                    total += 10 * digit;
+                }
+                if let Some(digit) = last_digit {
+                    total += digit;
                 }
             }
             
