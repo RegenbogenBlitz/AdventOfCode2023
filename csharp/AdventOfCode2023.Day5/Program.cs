4a9ff5c94b440f309f0bd22f7d2d2895fce74aef
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2023-12-15 20:54:14 +0000

    solve day 5 part 2 csharp

--- csharp/AdventOfCode2023.Day5/Program.cs
+++ csharp/AdventOfCode2023.Day5/Program.cs
@@ -5,8 +5,44 @@ public class Program
     public static long Part1(string filename)
     {
         var (seeds, mapSequence) = ReadFile(filename);
+        var combinedMap = mapSequence.CombineMaps();
         
-        return seeds.Select(s=> mapSequence.Covert(s)).Min();
+        return seeds.Select(s=> combinedMap.Covert(s)).Min();
+    }
+    
+    public static long Part2(string filename)
+    {
+        var (seeds, mapSequence) = ReadFile(filename);
+        var combinedMap = mapSequence.CombineMaps();
+        
+        var seedRanges = new List<(long start, long end)>();
+        for(int i = 0; i < seeds.Count; i++)
+        {
+            var seed = seeds.ElementAt(i);
+            var nextSeed = seeds.ElementAt(i + 1);
+            seedRanges.Add((seed, seed + nextSeed - 1));
+            i++;
+        }
+
+        var orderedMapLines = combinedMap.MapLines.OrderBy(m => m.DestinationStart).ToArray();
+        foreach (var mapLine in orderedMapLines)
+        {
+            var possibleSeeds = seedRanges
+                .Where(r =>
+                    r.start <= mapLine.SourceStart && mapLine.SourceStart <= r.end ||
+                    r.start <= mapLine.SourceEnd && mapLine.SourceEnd <= r.end ||
+                    mapLine.SourceStart <= r.start && r.start <= mapLine.SourceEnd ||
+                    mapLine.SourceStart <= r.end && r.end <= mapLine.SourceEnd
+                ).Select(r => Math.Max(r.start, mapLine.SourceStart))
+                .ToArray();
+
+            if (possibleSeeds.Any())
+            {
+                return mapLine.Covert(possibleSeeds.Min());
+            } 
+        }
+        
+        throw new Exception("No seed found");
     }
     
     public static (IReadOnlyCollection<long> seeds, MapSequence mapSequence) ReadFile(string filename)
@@ -39,7 +75,13 @@ public class Program
     }
 }
 
-public record MapLine(long DestinationStart, long SourceStart, long RangeLength);
+public record MapLine(long DestinationStart, long SourceStart, long RangeLength)
+{
+    public long SourceEnd => SourceStart + RangeLength - 1;
+    public long DestinationEnd => DestinationStart + RangeLength - 1;
+    public long Covert(long source) => source - SourceStart + DestinationStart;
+    public long Revert(long destination) => destination - DestinationStart + SourceStart;
+};
 
 public class Map
 {
@@ -56,7 +98,7 @@ public class Map
     public long Covert(long source)
     {
         var matchingLines = MapLines
-            .Where(l => l.SourceStart <= source && source <= l.SourceStart + l.RangeLength - 1)
+            .Where(l => l.SourceStart <= source && source <= l.SourceEnd)
             .ToArray();
 
         if (matchingLines.Count() > 1)
@@ -66,9 +108,7 @@ public class Map
         
         var matchingLine = matchingLines.SingleOrDefault();
         
-        return matchingLine is not null
-            ? source - matchingLine.SourceStart + matchingLine.DestinationStart
-            : source;
+        return matchingLine?.Covert(source) ?? source;
     }
 }
 
@@ -90,5 +130,111 @@ public class MapSequence
         }
 
         return result;
+    }
+
+    public Map CombineMaps()
+    {
+        if (Maps.Count == 0)
+        {
+            throw new Exception("No maps to combine");
+        }
+        
+        var currentMapLines = Maps.First().MapLines;
+        var nextMapLines = new List<MapLine>();
+
+        for (var i = 1; i < Maps.Count; i++)
+        {
+            var inMapLines = currentMapLines;
+            var outMapLines = Maps.ElementAt(i).MapLines;
+            
+            var maxInMapLineDestination = inMapLines.Max(m => m.DestinationEnd);
+            var maxOutMapLineSource = outMapLines.Max(m => m.SourceEnd);
+            var max = Math.Max(maxInMapLineDestination, maxOutMapLineSource);
+
+            for (long j = 0; j <= max; j++)
+            {
+                var inMapLine = inMapLines
+                    .SingleOrDefault(m => m.DestinationStart <= j && j <= m.DestinationEnd);
+
+                if (inMapLine is null)
+                {
+                    var minInMapLineDestination = inMapLines.Min(m => m.DestinationStart);
+
+                    if (j < minInMapLineDestination)
+                    {
+                        inMapLine = new MapLine(0, 0, minInMapLineDestination);
+                    }
+                    else if (j > maxInMapLineDestination)
+                    {
+                        inMapLine = new MapLine(
+                            maxInMapLineDestination + 1, 
+                            maxInMapLineDestination + 1,
+                            max - maxInMapLineDestination);
+                    }
+                    else
+                    {
+                        var priorInMap = inMapLines
+                            .Where(m => m.DestinationEnd < j)
+                            .OrderByDescending(m => m.DestinationEnd)
+                            .First();
+                        var nextInMap = inMapLines
+                            .Where(m => m.DestinationStart > j)
+                            .OrderBy(m => m.DestinationStart)
+                            .First();
+                        
+                        inMapLine = new MapLine(
+                            priorInMap.DestinationEnd + 1,
+                            priorInMap.DestinationEnd + 1,
+                            nextInMap.DestinationStart - priorInMap.DestinationEnd - 1);
+                    }
+                }
+
+                var outMapLine = outMapLines
+                    .SingleOrDefault(m => m.SourceStart <= j && j <= m.SourceEnd);
+
+                if (outMapLine is null)
+                {
+                    var minOutMapLineSource = outMapLines.Min(m => m.SourceStart);
+
+                    if (j < minOutMapLineSource)
+                    {
+                        outMapLine = new MapLine(0, 0, minOutMapLineSource);
+                    }
+                    else if (j > maxOutMapLineSource)
+                    {
+                        outMapLine = new MapLine(
+                            maxOutMapLineSource + 1, 
+                            maxOutMapLineSource + 1,
+                            max - maxOutMapLineSource);
+                    }
+                    else
+                    {
+                        var priorOutMap = outMapLines
+                            .Where(m => m.SourceEnd < j)
+                            .OrderByDescending(m => m.SourceEnd)
+                            .First();
+                        var nextOutMap = outMapLines
+                            .Where(m => m.SourceStart > j)
+                            .OrderBy(m => m.SourceStart)
+                            .First();
+                        
+                        outMapLine = new MapLine(
+                            priorOutMap.SourceEnd + 1,
+                            priorOutMap.SourceEnd + 1,
+                            nextOutMap.SourceStart - priorOutMap.SourceEnd - 1);
+                    }
+                }
+                var newRange = Math.Min(inMapLine.DestinationEnd - j, outMapLine.SourceEnd - j) + 1;
+                var newMapLine = new MapLine(outMapLine.Covert(j), inMapLine.Revert(j), newRange);
+                nextMapLines.Add(newMapLine);
+
+                j += newRange - 1;
+            }
+            
+            currentMapLines = nextMapLines;
+            nextMapLines = new List<MapLine>();
+        }
+        
+        return new Map("combined", currentMapLines);
     }
 }
--- csharp/AdventOfCode2023.Day5/UnitTest1.cs
+++ csharp/AdventOfCode2023.Day5/UnitTest1.cs
@@ -19,6 +19,22 @@ public class Tests
     }
     
     [Test]
+    public void Part2Example()
+    {
+        var location = Program.Part2("example.txt");
+        
+        Assert.That(location, Is.EqualTo(46));
+    }
+    
+    [Test]
+    public void Part2Solution()
+    {
+        var location = Program.Part2("input.txt");
+        
+        Assert.That(location, Is.EqualTo(59370572));
+    }
+    
+    [Test]
     public void ReadFile()
     {
         var (seeds, mapSequence) = Program.ReadFile("example.txt");
@@ -43,5 +59,269 @@ public class Tests
         Assert.That(humidityToLocation1.DestinationStart, Is.EqualTo(56));
         Assert.That(humidityToLocation1.SourceStart, Is.EqualTo(93));
         Assert.That(humidityToLocation1.RangeLength, Is.EqualTo(4));
+    }
+    
+    [Test]
+    public void CombineMaps_Trival()
+    {
+        var seeds = new[] { 0, 5, 9 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 10)
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(0, 0, 10)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_FirstMapHasNoStart()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5)
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(0, 0, 20)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_SecondMapHasNoStart()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 20)
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_FirstMapHasNoEnd()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 10)
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_SecondMapHasNoEnd()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5)
+            }),
+            new Map("map2", new[]
+            {
+                
+                new MapLine(0, 0, 10)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_FirstMapHasNoMiddle()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(20, 20, 10)
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5),
+                new MapLine(20, 20, 10)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_SecondMapHasNoMiddle()
+    {
+        var seeds = new[] { 10, 14, 15, 19 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(15, 10, 5),
+                new MapLine(10, 15, 5),
+                new MapLine(20, 20, 10)
+
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(0, 0, 10),
+                new MapLine(20, 20, 10)
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_OverlappingRanges()
+    {
+        var seeds = new[] { 0, 9, 10, 14, 15, 19, 20, 29, 30, 39, 40, 44, 45, 49, 50, 59 };
+        var mapSequence = new MapSequence(new[]
+        {
+            new Map("map1", new[]
+            {
+                new MapLine(15, 0, 15),
+                new MapLine(0, 15, 15),
+                
+                new MapLine(40, 30, 10),
+                new MapLine(50, 40, 10),
+                new MapLine(30, 50, 10),
+            }),
+            new Map("map2", new[]
+            {
+                new MapLine(10, 0, 10),
+                new MapLine(20, 10, 10),
+                new MapLine(0, 20, 10),
+                
+                new MapLine(45, 30, 15),
+                new MapLine(30, 45, 15),
+            })
+        });
+        
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_Example()
+    {
+        var (seeds, mapSequence) = Program.ReadFile("example.txt");
+
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
+    }
+    
+    [Test]
+    public void CombineMaps_Input()
+    {
+        var (seeds, mapSequence) = Program.ReadFile("input.txt");
+
+        var combinedMap = mapSequence.CombineMaps();
+
+        foreach (var seed in seeds)
+        {
+            var uncombined = mapSequence.Covert(seed);
+            var combined = combinedMap.Covert(seed);
+            Assert.That(uncombined, Is.EqualTo(combined));
+        }
     }
 }
